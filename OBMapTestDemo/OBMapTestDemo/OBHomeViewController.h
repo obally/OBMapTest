0c8e63670b8c4dcb36da66f88442215036348460
obally <573987088@qq.com>
2017-02-27 15:10:13 +0800

    add some button

---
+++ OBMapTestDemo/OBMapTestDemo/OBHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  OBHomeViewController.h
+//  OBMapTestDemo
+//
+//  Created by obally on 17/2/27.
+//  Copyright © 2017年 obally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OBHomeViewController : UIViewController
+
+@end
--- OBMapTestDemo/OBMapTestDemo/OBHomeViewController.m
+++ OBMapTestDemo/OBMapTestDemo/OBHomeViewController.m
@@ -1,31 +1,28 @@
 //
-//  ViewController.m
+//  OBHomeViewController.m
 //  OBMapTestDemo
 //
 //  Created by obally on 17/2/27.
 //  Copyright © 2017年 obally. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "OBHomeViewController.h"
-@interface ViewController ()
+
+@interface OBHomeViewController ()
 
 @end
 
-@implementation ViewController
+@implementation OBHomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(50, 100, 200, 50)];
-    button.backgroundColor = [UIColor redColor];
+    button.backgroundColor = [UIColor yellowColor];
+    [button setTitle:@"返回" forState:UIControlStateNormal];
     [self.view addSubview:button];
-    [button addTarget:self action:@selector(push) forControlEvents:UIControlEventTouchUpInside];
+    [button addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
     
-}
-- (void)push
-{
-    OBHomeViewController *home = [[OBHomeViewController alloc]init];
-    [self presentViewController:home animated:YES completion:nil];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +30,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)dismiss
+{
+    [self dismissViewControllerAnimated:YES
+                             completion:nil];
+}
 
 @end
